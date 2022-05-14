655696af4a61364b32c03090e8bc825f2fb72146
Fatih Kilic <me@fklc.dev>
2022-05-14 01:40:11 -0400

    Made improvements to updater

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const pino = require('pino');
 
 	state.logger.info('Starting');
 
-	await utils.checkVersion('v0.6.3');
+	await utils.checkVersion('v0.6.4');
 	state.logger.info('Update checked.');
 
 	await storage.initializeDB();
