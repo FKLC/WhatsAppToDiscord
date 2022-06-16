3efacfe7f51b8f88cb59f2293de525b918ce11a7
vishyvishal14 <75655494+vishyvishal14@users.noreply.github.com>
2022-06-16 19:40:54 +0530

    Just commented what each line does, im a weird nolifer

--- install_script.sh
+++ install_script.sh
@@ -3,18 +3,25 @@
 # i am bad at bash scripting but i tried to make a simple script, please run with root 
 # done by vishyvishal14
 # installing nodejs 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
 
+# update reps
 apt update && apt upgrade
-
+#pull node setup
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# install nodejs
+sudo apt-get install -y nodejs
+# update reps again 
+apt update && apt upgrade
+# clone the git
 git clone https://github.com/FKLC/WhatsAppToDiscord
-
+# get into that dir ;-;
 cd WhatsAppToDiscord
-
+#node version show
 echo "Your Node Version:"
 node -v
+# install required packages/dependencies
 npm install
+# run the program
 node .
 
 
