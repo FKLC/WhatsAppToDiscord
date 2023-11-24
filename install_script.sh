a860cc76ec0021df412ee8d9ac3289dd66099f81
Fatih Kilic <me@fklc.dev>
2023-11-23 21:25:51 -0500

    Small modifications to @vishalpj14's code.

--- install_script.sh
+++ install_script.sh
@@ -4,17 +4,10 @@
 # done by vishyvishal14
 # installing nodejs 
 
-#make it superuser mode so other commands below work properly 
-
-sudo su
-# update reps
-apt update && apt upgrade
 #pull node setup
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 # install nodejs
 sudo apt-get install -y nodejs
-# update reps again 
-apt update && apt upgrade
 # clone the git
 git clone https://github.com/FKLC/WhatsAppToDiscord
 # get into that dir ;-;
@@ -26,5 +19,3 @@ node -v
 npm install
 # run the program
 node .
-
-
