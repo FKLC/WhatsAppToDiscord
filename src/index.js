febfa53008acaf77aa1e3f05fe2c91f8199f9c7d
arespawn <sniperman_koukou@hotmail.com>
2025-07-29 10:07:35 +0300

    Update index.js
    Bump const version

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const storage = require('./storage.js');
 const whatsappHandler =  require('./whatsappHandler.js');
 
 (async () => {
-  const version = 'v0.10.30';
+  const version = 'v0.10.31';
   state.logger = pino({ mixin() { return { version }; } }, pino.destination('logs.txt'));
   let autoSaver = setInterval(() => storage.save(), 5 * 60 * 1000);
   ['SIGINT', 'uncaughtException', 'SIGTERM'].forEach((eventName) => process.on(eventName, async (err) => {
